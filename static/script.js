46e32c5aeedb79bff44fdd3e747b2aac18755a88
ajiways <ajiwayswc@gmail.com>
2022-04-13 00:44:06 +0300

    implemented static serving for view products page

---
+++ static/script.js
@@ -0,0 +1,94 @@
+const productsContainer = document.querySelector('.products');
+const suggestionContainer = document.querySelector('.suggestions');
+const suggestionsList = document.querySelector('.suggestions__items');
+const searchInput = document.querySelector('#search');
+
+function handleTyping() {
+  const searchDebaunced = debounce(search, 1000);
+  let prevValue = '';
+  searchInput.addEventListener('keyup', function (event) {
+    if (prevValue != event.target.value) {
+      searchDebaunced(event.target.value);
+      prevValue = event.target.value;
+    }
+  });
+}
+
+function debounce(cb, duration) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      cb(...args);
+    }, duration);
+  };
+}
+
+function search(searchValue) {
+  fetch(`http://localhost:3000/search?search=${searchValue}`, { method: 'GET' })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.hits.hits);
+      const prods = data.hits.hits.map((el) => el._source);
+      const suggestions = data.suggest.simple_phrase;
+      drawSuggestion(suggestions);
+      handleSuggestionClick();
+      drawProducts(prods);
+    });
+}
+
+function drawProducts(products) {
+  const result = products
+    .map((product) => {
+      const template = `
+      <div class="card product">
+        <h3>${product.name}</h3>
+        <p>
+          Цена: ${product.price}
+        </p>
+      </div>
+    `;
+      return template;
+    })
+    .join('');
+  productsContainer.innerHTML = result;
+}
+
+function handleSuggestionClick() {
+  document.querySelectorAll('.suggested').forEach((element) => {
+    element.addEventListener('click', function () {
+      const suggestion = this.getAttribute('data-suggestion');
+      searchInput.value = suggestion;
+      search(suggestion);
+    });
+  });
+}
+
+function drawSuggestion(suggestions) {
+  let options = [];
+  suggestions.forEach((el) => {
+    const templated = el.options.map((opt) => {
+      const optResult = `
+        <li >
+          <a class='suggested' href='#' data-suggestion='${opt.text}'>${opt.highlighted}</a>
+        </li>
+      `;
+      return optResult;
+    });
+
+    if (templated.length > 0) {
+      options.push(templated.join('<li> или </li>'));
+    }
+  });
+
+  suggestionsList.innerHTML = options;
+  if (options.length > 0) {
+    suggestionContainer.classList.add('visible');
+  } else {
+    suggestionContainer.classList.remove('visible');
+  }
+}
+
+handleTyping();
